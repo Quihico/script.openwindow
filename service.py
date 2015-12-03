8773293326e3b3f6c6452d34528cddf7f5f7384e
totalrevolution <info@totalrevolution.tv>
2015-12-03 11:50:32 +0000

    Fixed issue with white screen if box is powered down during update process. Also fixed heading for skip registration.

--- service.py
+++ service.py
@@ -5,6 +5,17 @@ ADDON                      = xbmcaddon.Addon(ADDONID)
 ADDONS                     = xbmc.translatePath(os.path.join('special://home','addons',''))
 HOME                       = xbmc.translatePath('special://home/')
 RestoreGUI                 = os.path.join(HOME,'userdata','addon_data','service.openelec.settings','restoregui')
+RunWizard                  = os.path.join(HOME,'userdata','addon_data','service.openelec.settings','runwizard')
+
+if os.path.exists(RunWizard):
+    xbmc.executebuiltin('RunAddon(script.openwindow)')
+    try:
+        shutil.rmtree(RunWizard)
+    except:
+        pass
 
 if os.path.exists(RestoreGUI):
-    xbmc.executebuiltin('Skin.SetString(Branding,off)')
+    try:
+        xbmc.executebuiltin('Skin.SetString(Branding,off)')
+    except:
+        pass
